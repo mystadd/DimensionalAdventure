1833eacfec1f29f54af936bfc30eebd5154dec08
mystadd <di.mystadd@gmail.com>
2025-01-13 20:40:55 +0300

    Первый коммит: загрузка Unity-проекта

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+	public Door door;
+	KeyHold keyHold;
+	public GameObject keys;
+
+	private void Awake()
+	{
+		keyHold = FindObjectOfType<KeyHold>();
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			if (keyHold.key == 1)
+			{
+				door.Open();
+				Destroy(keys);
+				keyHold.key--;
+			}
+		}
+
+		if (keyHold.key > 1)
+		{
+			keyHold.key = 1;
+		}
+
+		if (keyHold.key < 0)
+		{
+			keyHold.key = 0;
+		}
+	}
+
+	public void OnTriggerExit2D(Collider2D col)
+	{
+        if (col.tag == "Player" && keyHold.hasKey == true)
+        {
+            door.Close();
+        }
+    }
+}
